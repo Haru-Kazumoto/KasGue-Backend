e5fae92d5d48ca44c79decd7e5cb8f211f83935c
Haru-Kazumoto <jiaathaya@gmail.com>
2025-09-26 17:35:54 +0700

    add: pg dep

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import express from "express";
+import pkg from "pg";
+const { Pool } = pkg;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+})
+
 app.use(express.json());
 
-app.get('/check', function(req, response) {
+app.get('/check/server', function(req, response) {
     response.send('Server is running');
+});
+
+app.get("/check/db", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows);
 });
 
 app.listen(PORT, function() {
